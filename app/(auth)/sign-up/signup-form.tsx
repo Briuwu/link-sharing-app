99881d2c76fdffe58f916da2894fe2d2787afd2c
Briuwu <millontebrian@gmail.com>
2025-03-29 19:19:35 +0800

    add validation schema and error handling to SignUpForm component

--- app/(auth)/sign-up/signup-form.tsx
+++ app/(auth)/sign-up/signup-form.tsx
@@ -2,13 +2,40 @@
 
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useForm } from "@tanstack/react-form";
+import { AnyFieldApi, useForm } from "@tanstack/react-form";
 
 import emailIcon from "@/public/assets/images/icon-email.svg";
 import passwordIcon from "@/public/assets/images/icon-password.svg";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { z } from "zod";
+
+const signUpSchema = z
+  .object({
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(8, "Password must be at least 8 characters long"),
+    confirmPassword: z
+      .string()
+      .min(8, "Password must be at least 8 characters long"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+  });
+
+function FieldInfo({ field }: { field: AnyFieldApi }) {
+  return (
+    <>
+      {field.state.meta.isTouched && field.state.meta.errors.length ? (
+        <em className="text-red text-xs">
+          {field.state.meta.errors.map((error) => error.message).join(", ")}
+        </em>
+      ) : null}
+      {field.state.meta.isValidating ? "Validating..." : null}
+    </>
+  );
+}
 
 export const SignUpForm = () => {
   const form = useForm({
@@ -19,6 +46,9 @@ export const SignUpForm = () => {
     },
     onSubmit: ({ value }) => {
       console.log(value);
+    },
+    validators: {
+      onChange: signUpSchema,
     },
   });
   return (
@@ -46,6 +76,7 @@ export const SignUpForm = () => {
               alt=""
               className="absolute top-7.5 left-2.5"
             />
+            <FieldInfo field={field} />
           </div>
         )}
       </form.Field>
@@ -59,13 +90,14 @@ export const SignUpForm = () => {
               value={field.state.value}
               onChange={(e) => field.handleChange(e.target.value)}
               className="rounded pl-7.5"
-              placeholder="Enter your password"
+              placeholder="At least 8 characters"
             />
             <Image
               src={passwordIcon}
               alt=""
               className="absolute top-7.5 left-2.5"
             />
+            <FieldInfo field={field} />
           </div>
         )}
       </form.Field>
@@ -74,22 +106,27 @@ export const SignUpForm = () => {
           <div className="relative space-y-1">
             <Label className="text-charcoal text-xs">Confirm Password</Label>
             <Input
-              id="password"
+              id="confirmPassword"
               type="password"
               value={field.state.value}
               onChange={(e) => field.handleChange(e.target.value)}
               className="rounded pl-7.5"
-              placeholder="Enter your password"
+              placeholder="At least 8 characters"
             />
             <Image
               src={passwordIcon}
               alt=""
               className="absolute top-7.5 left-2.5"
             />
+            <FieldInfo field={field} />
           </div>
         )}
       </form.Field>
-      <Button type="submit" className="bg-indigo w-full font-semibold">
+      <Button
+        type="submit"
+        className="bg-indigo w-full font-semibold"
+        onClick={form.handleSubmit}
+      >
         Login
       </Button>
       <p className="text-grey-dark mx-auto w-fit">
