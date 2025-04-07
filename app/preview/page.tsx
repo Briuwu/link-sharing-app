86cbc05cd1dbab015d5ea9d9dc20c9d8bb5674c3
Briuwu <millontebrian@gmail.com>
2025-04-07 16:54:06 +0800

    fix: update ShareLinkBtn URL to use user_id instead of id

--- app/preview/page.tsx
+++ app/preview/page.tsx
@@ -29,8 +29,8 @@ export default async function PreviewPage() {
         <ShareLinkBtn
           url={
             process.env.NODE_ENV === "production"
-              ? `https://devlinks-uwu.vercel.app/preview/${details?.id}`
-              : `http://localhost:3000/preview/${details?.id}`
+              ? `https://devlinks-uwu.vercel.app/preview/${details?.user_id}`
+              : `http://localhost:3000/preview/${details?.user_id}`
           }
         />
       </header>
